8dd641854cec0fb2b305817fd5dbd239148f33ed
jarvis73 <zjw.math@qq.com>
2018-11-12 14:12:25 +0800

    Add network checking

--- scripts/GetBingWallPaper.ps1
+++ scripts/GetBingWallPaper.ps1
@@ -1,18 +1,47 @@
-Write-Host "Updating wallpaper...`n"
+#########################################################################
+#
+# Copyright © 2018 - 2019 Jianwei ZHANG
+# All rights reserved.
+#
+# Info: Download Bing everyday picture and set as wallpaper.
+#
+# Modified:
+#   * Initialization
+#   * Print running information
+#   * Check network and restart every one minute if failed
+#
+##########################################################################
+
+function print($info)
+{
+    Get-Date -UFormat "%H:%M:%S " |write-host -NoNewline
+    Write-Host $info    
+}
+
+print("Updating wallpaper...")
 
 # Request for Bing wallpaper XML file
-[xml]$reponse = Invoke-WebRequest -Uri "https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1" -UseBasicParsing
+while (1)
+{
+    try {
+        [xml]$reponse = Invoke-WebRequest -Uri "https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1" -UseBasicParsing
+        break
+    }
+    catch [System.Net.WebException]{
+        print("Failed! Please check the network!")
+        Start-Sleep -Seconds 60
+    }
+}
 
 # Extract date and image url
 $image = $reponse.images.image
-$startdate = $image.startdate
-$bingImageURL = "https://cn.bing.com" + $image.url
-$bingImageName = $image.urlBase.split("/")[-1].split("_")[0] + "." + $image.url.split(".")[-1]
+$enddate = $image.enddate
+$bingImageURL = "https://cn.bing.com" + $image.urlBase + "_1920x1080.jpg"
+$bingImageName = $enddate.Substring(6, 2) + "_" + $image.urlBase.split("/")[-1].split("_")[0] + "." + $image.url.split(".")[-1]
 
 # Check folders
 $wallPaperPath = [System.Environment]::GetFolderPath("MyPictures") + "\EverydayBingWallPapers"
-
-$monthWallPaperPath = ($wallPaperPath, $startdate.Substring(0, 4), $startdate.Substring(4, 2)) -join "\"
+$monthWallPaperPath = ($wallPaperPath, $enddate.Substring(0, 4), $enddate.Substring(4, 2)) -join "\"
 if (!(Test-Path $monthWallPaperPath))
 {
     $null = New-Item $monthWallPaperPath -ItemType "directory"
@@ -23,13 +52,21 @@ $wallPaperFile = ($monthWallPaperPath, $bingImageName) -join "\"
 # Download and save image
 if (!(Test-Path $wallPaperFile))
 {
+    print("Trying download image...")
     Invoke-WebRequest $bingImageURL -OutFile $wallPaperFile
+
+    # If failed, retry every 60 seconds
+    while (!(Test-Path $wallPaperFile))
+    {
+        Start-Sleep -Seconds 60
+        Invoke-WebRequest $bingImageURL -OutFile $wallPaperFile
+    }
 }
 
 # Set wallpaper
 . "$PSScriptRoot\SetWallPaper.ps1"
 Set-Wallpaper -Path $wallPaperFile
 
-Write-Host "Finished!"
+print("Finished!")
 
 exit
